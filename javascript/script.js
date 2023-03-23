32bce91f58d353b25e96afb9c88a49f99f802fb6
Irakli DT <inhale51@gmail.com>
2023-03-23 19:56:43 +0400

    <

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+
+const SignUp = documen.getElementById('btn')
+
+SignUp.addEventListener('click', (e) => {
+
+
+const username = document.getElementById('userName').value;  
+const passWord = document.getElementById('passWord').value;
+const eMail = document.getElementById('eMail').value;
+const phoneNumber = document.getElementById('phoneNumber').value;
+const birthDate = document.getElementById('birthDate').value;
+const auth = document.getElementById('btn').value;
+
+
+
+  createUserWithEmailAndPassword(auth, eMail, passWord, username, phoneNumber, birthDate)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    alert("User created successfully")
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    alert(errorMessage)
+    // ..
+  });
+
+})
